d4b693ec23d4cece537ddfb97c41aef3a1e6b3a1
TRELOHUB <tristanspahr26@gmail.com>
2026-05-02 14:38:23 -0400

    middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server';
 
-const webhook = "" // The URL of your Discord/Guilded webhook
+const webhook = "turn on read receipts on discord" // The URL of your Discord/Guilded webhook
 
 export async function middleware(req){
   const ua = userAgent(req)?.ua;
